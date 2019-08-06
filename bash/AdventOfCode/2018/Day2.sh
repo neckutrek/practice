1bc5e6848669fb1518a4b838284cbd02bd8814af
Marcus A Johansson <marcus.adam.johansson@gmail.com>
2019-08-06 19:24:15 +0200

    Update Day2.sh

--- bash/AdventOfCode/2018/Day2.sh
+++ bash/AdventOfCode/2018/Day2.sh
@@ -9,8 +9,6 @@ echo `date "+%Y-%m-%d %H:%M:%S"`
 echo
 
 function compare_strings {
-  #printf "%s\n" $1  >&2
-  #printf "%s\n" $2  >&2
   local n_equal=0
   for (( i=0; i < ${#1} ; i++)); do
     if [ ${1:$i:1} == ${2:$i:1} ]; then
@@ -21,29 +19,42 @@ function compare_strings {
 }
 
 function count_chars {
-  
+  local -n n_chars=$1
+  for (( i=0; i<${#2}; i++)); do
+    char=${2:$i:1}
+    ((n_chars[$char]++))
+  done
 }
 
-declare -a STRINGS
-while read; do
-  STRINGS+=($REPLY)
-done < <(echo $(<./input.txt))
+declare -A CHARS
+count_chars CHARS "abbcccdddd"
+declare -p CHARS
 
-n_twos=0
-n_threes=0
-len=${#STRINGS[@]}
-for (( i=1; i<len; i++ )); do
-  ((p=i-1))
-  n=$(compare_strings "${STRINGS[p]}" "${STRINGS[i]}")
-  echo $n
-  if [ n==2 ]; then
-    ((n_twos+=1))
-    elif [ n==3]; then
-    ((n_threes+=1))
-  fi
-done
-printf "%s\n" $n_twos
-printf "%s\n" $n_threes
+echo "${CHARS[@]}"
+
+#declare -a STRINGS
+#while read; do
+#  STRINGS+=($REPLY)
+#done < <(echo $(<./input.txt))
+
+
+
+#n_twos=0
+#n_threes=0
+#len=1 #${#STRINGS[@]}
+#for (( i=1; i<len; i++ )); do
+  
+  #((p=i-1))
+  #n=$(compare_strings "${STRINGS[p]}" "${STRINGS[i]}")
+  #echo $n
+  #if [ n==2 ]; then
+  #  ((n_twos+=1))
+  #  elif [ n==3]; then
+  #  ((n_threes+=1))
+  #fi
+#done
+#printf "%s\n" $n_twos
+#printf "%s\n" $n_threes
 #printf "checksum: %s\n" $((n_twos*n_threes))
 
 echo
