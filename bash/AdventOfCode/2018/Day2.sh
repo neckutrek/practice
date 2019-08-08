ad1dbaf9fb613eae69969a72fe5dca5030980096
Marcus A Johansson <marcus.adam.johansson@gmail.com>
2019-08-08 09:24:22 +0200

    Update Day2.sh

--- bash/AdventOfCode/2018/Day2.sh
+++ bash/AdventOfCode/2018/Day2.sh
@@ -5,28 +5,6 @@
 #  - print from within function: printf "%s\n" $msg >&2
 #  - expanding an array only gives the first argument, use "${array[@]}" instead
 
-echo "- - - START - - -"
-echo `date "+%Y-%m-%d %H:%M:%S"`
-echo
-
-function compare_strings {
-  local n_equal=0
-  for (( i=0; i < ${#1} ; i++)); do
-    if [ ${1:$i:1} == ${2:$i:1} ]; then
-      ((n_equal+=1))
-    fi
-  done
-  echo $n_equal
-}
-
-function count_chars {
-  local -n n_chars=$1
-  for (( i=0; i<${#2}; i++)); do
-    char=${2:$i:1}
-    ((n_chars[$char]++))
-  done
-}
-
 function get_checksum {
   local arr=("$@")
 
@@ -60,35 +38,20 @@ function get_checksum {
   echo "$((twos*threes))"
 }
 
-#declare -A CHARS
-#count_chars CHARS "abbcccdddd"
-#declare -p CHARS
+function main {
+  echo "- - - START - - -"
+  echo `date "+%Y-%m-%d %H:%M:%S"`
+  echo
 
-declare -a strings
-while read; do
-  strings+=($REPLY)
-done < <(echo $(<./input.txt))
+  declare -a strings
+  while read; do
+    strings+=($REPLY)
+  done < <(echo $(<./input.txt))
 
-echo $(get_checksum "${strings[@]}")
+  echo $(get_checksum "${strings[@]}")
 
+  echo
+  echo "- - - FINAL - - -"
+}
 
-#n_twos=0
-#n_threes=0
-#len=1 #${#STRINGS[@]}
-#for (( i=1; i<len; i++ )); do
-  
-  #((p=i-1))
-  #n=$(compare_strings "${STRINGS[p]}" "${STRINGS[i]}")
-  #echo $n
-  #if [ n==2 ]; then
-  #  ((n_twos+=1))
-  #  elif [ n==3]; then
-  #  ((n_threes+=1))
-  #fi
-#done
-#printf "%s\n" $n_twos
-#printf "%s\n" $n_threes
-#printf "checksum: %s\n" $((n_twos*n_threes))
-
-echo
-echo "- - - FINAL - - -"
+[[ $_ != $0 ]] && main "$@"
