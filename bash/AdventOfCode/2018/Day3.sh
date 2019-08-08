3872d1c8e3139ac310c33d3a5347d73caac3bef7
Marcus A Johansson <marcus.adam.johansson@gmail.com>
2019-08-08 14:35:59 +0200

    Update Day3.sh

--- bash/AdventOfCode/2018/Day3.sh
+++ bash/AdventOfCode/2018/Day3.sh
@@ -3,8 +3,24 @@
 # lessons learned:
 #  - 
 
+function extract_square_info {
+  echo "$1" >&2
+}
+
 function main {
-  echo "hello world!"
+  echo "- - - START - - -"
+  echo `date "+%Y-%m-%d %H:%M:%S"`
+  echo
+
+  declare -a strings
+  while read; do
+    strings+=($REPLY)
+  done < <(echo $(<./input.txt))
+
+  echo $(extract_square_info "${strings[0]}")
+
+  echo
+  echo "- - - FINAL - - -"
 }
 
 [[ $_ != $0 ]] && main "$@"
