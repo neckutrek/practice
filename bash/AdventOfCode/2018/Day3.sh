659e48c5b3e82901d91fd9d4d6ea80aabaf8ec62
Marcus A Johansson <marcus.adam.johansson@gmail.com>
2019-08-14 11:31:49 +0200

    Update Day3.sh

--- bash/AdventOfCode/2018/Day3.sh
+++ bash/AdventOfCode/2018/Day3.sh
@@ -4,71 +4,38 @@
 #  - return multiple values from function: printf "(%s %s ...)" val1 val2 ... ; (array initialization)
 #  - receive string with spaces in it inside function: local -a arg=("${!1}")
 
-declare -a "FABRIC"
+export LC_ALL=C # disables unicode support, to improve performance
 
-function xy2idx { 
-  echo $(( $1 + $2 * 1000 )) 
-}
-
-function idx2xy {
-  printf "( %s %s )" \
-    $(( $1 % 1000 )) \
-    $(( $1 / 1000 ))
-}
-
-# input:  "#<id> @ <left>,<top>: <width>x<height>"
-# output: ( <left> <top> <width> <height> )
-function extract_square_info {
-  local -a arg=("${!1}")
-  printf "(%s %s %s %s)" \
-    $(sed -r 's/.* ([0-9]+),.*/\1/g' <<< $arg) \
-    $(sed -r 's/.*,([0-9]+):.*/\1/g' <<< $arg) \
-    $(sed -r 's/.* ([0-9]+)x.*/\1/g' <<< $arg) \
-    $(sed -r 's/.*x([0-9]+)$/\1/g' <<< $arg)
-}
-#'''
+declare -A "FABRIC" # associative array is faster in Bash than indexed array (when overwriting)
 
 # input:  2 claims as strings
 # output: 
 # modifies: array FABRIC
 function claim_fabric {
-  local -a arg1=("${!1}")
-  local -a arg2=("${!2}")
+  local -a c1=$1
+  local -a c2=$2
 
-  #echo "$arg1" >&2
-  #echo "$arg2" >&2
-  #echo "FABRIC: ${FABRIC[@]}" >&2
+  echo "c1: ${#c1[@]}   ${c1[@]}" >&2
+  echo "c2: ${#c2[@]}   ${c2[@]}" >&2
+  echo "FABRIC: ${FABRIC[@]}" >&2
 
-  local -a claim1=$(extract_square_info arg1)
-  local -a claim2=$(extract_square_info arg2)
-
-  local ax1="${claim1[0]}"
-  local ax2=$((claim1[0]+claim1[2]))
-  local ay1="${claim1[1]}"
-  local ay2=$((claim1[1]+claim1[3]))
-
-  local bx1="${claim2[0]}"
-  local bx2=$((claim2[0]+claim2[2]))
-  local by1="${claim2[1]}"
-  local by2=$((claim2[1]+claim2[3]))
-  
   local w1=0
   local w2=0
   local h1=0
   local h2=0
 
-  if [[ "$bx1" < "$ax2" && "$bx2" > "$ax1" ]]; then
-    if [[ "$bx1" > "$ax1" ]]; then w1=$bx1
-    else w1=$ax1; fi
-    if [[ "$bx2" < "$ax2" ]]; then w2=$bx2
-    else w2=$ax2; fi
+  if [[ "${c2[0]}" < "${c1[2]}" && "${c2[2]}" > "${c1[0]}" ]]; then
+    if [[ "${c2[0]}" > "${c1[0]}" ]]; then w1="${c2[0]}"
+    else w1="${c1[0]}"; fi
+    if [[ "${c2[2]}" < "${c1[2]}" ]]; then w2="${c2[2]}"
+    else w2="${c1[2]}"; fi
   fi
 
-  if [[ "$by1" < "$ay2" && "$by2" > "$ay1" ]]; then
-    if [[ "$by1" > "$ay1" ]]; then h1=$by1
-    else h1=$ay1; fi
-    if [[ "$by2" < "$ay2" ]]; then h2=$by2
-    else h2=$ay2; fi
+  if [[ "${c2[1]}" < "${c1[3]}" && "${c2[3]}" > "${c1[1]}" ]]; then
+    if [[ "$by1" > "${c1[1]}" ]]; then h1="${c2[1]}"
+    else h1="${c1[1]}"; fi
+    if [[ "${c2[3]}" < "${c1[3]}" ]]; then h2="${c2[3]}"
+    else h2="${c1[3]}"; fi
   fi
 
   if [[ "$w1" != 0 && "$w2" != 0 && \
@@ -76,19 +43,15 @@ function claim_fabric {
     local i j
     for (( i=w1; i<w2; i++ )); do
       for (( j=h1; j<h2; j++ )); do
-        local idx=$(xy2idx $i $j)
-        if [[ ! "${FABRIC[@]}" =~ "${idx}" ]]; then
-          FABRIC+=("$idx")
-        #else
-          #echo "not $i $j $idx" >&2
-        fi
+        local idx="x${i}x${j}"
+        [[ -v FABRIC["$idx"] ]] || FABRIC["$idx"]+="x"
       done
-    done
+    done 
   fi
   
-  #echo "FABRIC: ${FABRIC[@]}" >&2
+  echo "FABRIC: ${FABRIC[@]}" >&2
 
-  #echo "=====" >&2
+  echo "=====" >&2
 }
 
 # input:  set of all claims as array of strings
@@ -98,34 +61,46 @@ function total_overlap_fabric {
 
   local i j
   for (( i=0; i<"${#args[@]}"-1; i++ )); do
-    
     for (( j=i+1; j<"${#args[@]}"; j++ )); do
-      echo "$i $j " >&2
-      claim_fabric args["$i"] args["$j"]
+      claim_fabric "${args[$i]}" "${args[$j]}"
     done
   done
 
   echo "${#FABRIC[@]}"
 }
 
+# input:  "#<id> @ <left>,<top>: <width>x<height>"
+# output: ( <x1> <y1> <x2> <y2> )
+function extract_claim {
+  local -a data=$(printf "(%s %s %s %s)" \
+    $(sed -r 's/.* ([0-9]+),.*/\1/g' <<< $1) \
+    $(sed -r 's/.*,([0-9]+):.*/\1/g' <<< $1) \
+    $(sed -r 's/.* ([0-9]+)x.*/\1/g' <<< $1) \
+    $(sed -r 's/.*x([0-9]+)$/\1/g' <<< $1))
+
+  printf "(%s %s %s %s)" \
+    "${data[0]}" "${data[1]}" \
+    $((data[0]+data[2])) $((data[1]+data[3]))
+}
+
 function main {
   echo "- - - START - - -"
   echo `date "+%Y-%m-%d %H:%M:%S"`
 
-  declare -a strings
+  declare -a claims
   IFS="\n"
   while read; do
-    strings+=($REPLY)
-  done < <(echo $(<./input.txt))
+    claims+=($(extract_claim $REPLY))
+  done < <(echo $(<./input2.txt))
   unset IFS
-  echo "Loaded ${#strings[@]} strings."
-  echo 
+  echo
 
-  printf "Total overlapping fabric: %s square inches\n" $(total_overlap_fabric strings[@])
+  total_overlap_fabric claims[@]
+  
+  #printf "Total overlapping fabric: %s square inches\n" $(total_overlap_fabric strings[@])
 
   echo
   echo "- - - FINAL - - -"
 }
 
 [[ $_ != $0 ]] && main "$@"
-
