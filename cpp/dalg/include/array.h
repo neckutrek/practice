091e4ab5d98181edca0e9cee6db3cc3f7392b7d4
neckutrek <neckutrek@Marcuss-MBP.lan>
2019-12-04 07:25:48 +0100

    Completed array.h

--- cpp/dalg/include/array.h
+++ cpp/dalg/include/array.h
@@ -1,14 +1,22 @@
 #ifndef __NECKU_ARRAY_H__
 #define __NECKU_ARRAY_H__
 
-template<typename T, int SIZE>
+template<typename T, size_t SIZE>
 class Array
 {
 public:
-    
+
+    size_t      size() { return SIZE; }
+
+    T&          operator[](size_t i)
+                {
+                    assert( i >= 0 );
+                    assert( i < SIZE );
+                    return m_data[i];
+                }
 
 private:
-    T m_data[SIZE];
+    T           m_data[SIZE];
 
 };
 
--- cpp/dalg/test/test_array.cpp
+++ cpp/dalg/test/test_array.cpp
@@ -1,12 +1,16 @@
 #include "catch.hpp"
+#include "array.h"
 
-unsigned int Factorial( unsigned int number ) {
-    return number <= 1 ? number : Factorial(number-1)*number;
-}
+TEST_CASE( "Stack-allocated array", "[stack],[array]" ) {
+    
+    Array<int, 10> arr;
+    for (int i=0; i<arr.size(); ++i)
+    {
+        arr[i] = i;
+    }
 
-TEST_CASE( "Factorials are computed", "[factorial]" ) {
-    REQUIRE( Factorial(1) == 1 );
-    REQUIRE( Factorial(2) == 2 );
-    REQUIRE( Factorial(3) == 6 );
-    REQUIRE( Factorial(10) == 3628800 );
+    for (int i=0; i<arr.size(); ++i)
+    {
+        REQUIRE( arr[i] == i );
+    }
 }
